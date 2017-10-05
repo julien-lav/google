aadae0071882b7e3a44c377419bd32a6995bc206
julien <julie.laville@hotmail.fr>
2017-10-05 17:37:03 +0200

    Ajout exercice grille

--- js/app.js
+++ js/app.js
@@ -8,7 +8,6 @@ centralButton.onclick = function() {
   this.style.visibility = "hidden";
   block.style.visibility = "visible";
 }
-
 setTimeout(function() {
 	alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
 },10000);
---
+++ js/app_2.js
@@ -0,0 +1,25 @@
+
+
+var n = prompt('entrer une valeur : ');
+var square = document.getElementById("square");
+var bigSquare = document.getElementsByClassName("bigSquare");
+
+while(n < 1 || n > 71){
+	var n = prompt('entrer une valeur : ');}
+	
+	var number = parseInt(n);
+	Math.floor(number);
+	console.log(number);
+    
+    let size = (500 -(number*number))/number;
+    
+	//square.text('<div width='+size +' height=' +size+'>' +'</div>');	
+    console.log(size);
+
+	for(var i = 0; i < number*number ; i++)
+	{
+		//console.log(i);
+		console.log(size);
+		square.innerHTML += '<p style="width:'+size+'px; height:'+size+'px";> test' + '</p>'; 
+	}
+
